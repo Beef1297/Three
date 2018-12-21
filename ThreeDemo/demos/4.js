c28078824d364759b8ab7dedf35f56c3edbfbed7
Keigo Ushiyama <u1610096@edu.cc.uec.ac.jp>
2018-12-21 18:50:54 +0900

    material blending and slow vibration

--- ThreeDemo/demos/4.js
+++ ThreeDemo/demos/4.js
@@ -9,12 +9,14 @@ const camera = getPerspectiveCamera(45);
 let scene = new THREE.Scene();
 let gui;
 const cubeGroup = new THREE.Group();
+let theta = 0;
 /* methods field */
 function init() {
 
     camera.position.set(50, 20, 10);
-    camera.near = 10;
+    camera.near = 50;
     camera.far = 110;
+    camera.updateProjectionMatrix();
     camera.lookAt(scene.position);
     let controls = new function () {
         this.cameraNear = camera.near;
@@ -23,7 +25,17 @@ function init() {
            const cubeSize = Math.floor(Math.random() * 5);
            const cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
            const cubeMaterial = new THREE.MeshDepthMaterial();
-           const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+           const colorMaterial = new THREE.MeshBasicMaterial({
+               color: 0x00ee00,
+               transparent: true,
+               blending: THREE.MultiplyBlending,
+           });
+           const materials = [
+               cubeMaterial,
+               colorMaterial,
+           ];
+           const cube = new THREE.SceneUtils.createMultiMaterialObject(cubeGeometry, materials);
+           cube.children[0].scale.set(0.99, 0.99, 0.99);
            cube.castShadow = true;
            cube.position.set(-50 + 40 * Math.random(), 20 * Math.random() - 10, 100 * Math.random() - 50);
            cubeGroup.add(cube);
@@ -49,15 +61,17 @@ function init() {
     gui.add(controls, 'removeCube');
     gui.add(controls, 'Objects').listen();
 
-    scene.fog = new THREE.FogExp2(0xff55ff, 0.015);
     scene.add(cubeGroup);
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 100; i++) {
         controls.addCube();
     }
 }
 
 function draw() {
-
+    cubeGroup.traverse(function(e) {
+        e.position.y = e.position.y + 0.01 * Math.sin(theta);
+    });
+    theta += Math.PI / 180;
     renderer.render(scene, camera);
 }
 
