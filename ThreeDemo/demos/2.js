055b245627c1456cebf293b9a89872546327383e
Keigo Ushiyama <u1610096@edu.cc.uec.ac.jp>
2018-12-21 16:37:53 +0900

    add 2.js

--- ThreeDemo/demos/2.js
+++ ThreeDemo/demos/2.js
@@ -2,15 +2,27 @@ import {renderer, getPerspectiveCamera} from "../modules/three.js";
 import THREE from "../libs/three-looper.js"; // this lib is from Jaume Sanchez.
 import {dat} from "../libs/dat.gui.js"
 
+/* variable field */
 const canvas = renderer.domElement;
 const camera = getPerspectiveCamera(45);
 let scene = new THREE.Scene();
 
+/* methods field */
 function init() {
     const axes = new THREE.AxesHelper(30);
     scene.add(axes);
-    camera.position.set(0, 0, -10);
+    camera.position.set(30, 40, 30);
     camera.lookAt(scene.position);
+    const planeGeometry = new THREE.PlaneGeometry(60, 20);
+    const planeMaterial  = new THREE.MeshLambertMaterial({
+        color: 0x55ef22
+    });
+    const ambientLight = new THREE.AmbientLight(0xeeeeee, 1);
+    scene.add(ambientLight);
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.rotation.x = - Math.PI / 2;
+    scene.add(plane);
+    scene.fog = new THREE.FogExp2(0xff55ff, 0.015);
 }
 
 function draw() {
